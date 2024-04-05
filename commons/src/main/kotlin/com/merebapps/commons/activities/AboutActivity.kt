dc046c1cb3c0b5d64f094474f87c239d7b0c862b
adika6 <79251059+adika6@users.noreply.github.com>
2024-04-05 16:36:03 +0300

    Update AboutActivity.kt

--- commons/src/main/kotlin/com/merebapps/commons/activities/AboutActivity.kt
+++ commons/src/main/kotlin/com/merebapps/commons/activities/AboutActivity.kt
@@ -44,14 +44,31 @@ class AboutActivity : ComponentActivity() {
             val context = LocalContext.current
             val resources = context.resources
             AppThemeSurface {
-                val showExternalLinks = false
+                val showExternalLinks = remember { !resources.getBoolean(R.bool.hide_all_external_links) }
                 val showGoogleRelations = remember { !resources.getBoolean(R.bool.hide_google_relations) }
                 val onEmailClickAlertDialogState = getOnEmailClickAlertDialogState()
-               // val rateStarsAlertDialogState = getRateStarsAlertDialogState()
+                val rateStarsAlertDialogState = getRateStarsAlertDialogState()
                 val onRateUsClickAlertDialogState = getOnRateUsClickAlertDialogState(rateStarsAlertDialogState::show)
                 AboutScreen(
                     goBack = ::finish,
-                    
+                    helpUsSection = {
+                        val showHelpUsSection =
+                            remember { showGoogleRelations || !showExternalLinks }
+                        HelpUsSection(
+                            onRateUsClick = {
+                                onRateUsClick(
+                                    showConfirmationAdvancedDialog = onRateUsClickAlertDialogState::show,
+                                    showRateStarsDialog = rateStarsAlertDialogState::show
+                                )
+                            },
+                            onInviteClick = ::onInviteClick,
+                            onContributorsClick = ::onContributorsClick,
+                            showDonate = resources.getBoolean(R.bool.show_donate_in_about) && showExternalLinks,
+                            onDonateClick = ::onDonateClick,
+                            showInvite = showHelpUsSection,
+                            showRateUs = showHelpUsSection
+                        )
+                    },
                     aboutSection = {
                         val setupFAQ = rememberFAQ()
                         if (!showExternalLinks || setupFAQ) {
@@ -284,7 +301,7 @@ class AboutActivity : ComponentActivity() {
 
     private fun onPrivacyPolicyClick() {
         val appId = baseConfig.appId.removeSuffix(".debug").removeSuffix(".pro").removePrefix("com.merebapps.")
-        val url = "https://merebapps.com/privacy-policy-clock/"
+        val url = "https://merebapps.com/privacy/$appId.txt"
         launchViewIntent(url)
     }
 
